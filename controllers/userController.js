416553b9af373a86753b5283c4c054a44c22624c
Deenu <hansamadeenu086@gmail.com>
2026-05-03 15:32:31 +0530

    enc JWT update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/user.js"
 import bcrypt from "bcrypt"; 
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
 
 export function createUser(req,res){
     if (req.body.role == "admin"){
@@ -65,7 +66,7 @@ export function loginUser(req,res){
                             role : user.role,
                             img : user.img
                         },
-                        "deenu-first"
+                        process.env.JWT_KEY,
                     )
 
                     res.json({

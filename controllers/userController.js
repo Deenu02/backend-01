5138f7af9cf5d72f6f279db70dd214485df8c925
Deenu <hansamadeenu086@gmail.com>
2026-05-06 16:34:27 +0530

    role created

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,7 +71,8 @@ export function loginUser(req,res){
 
                     res.json({
                         message : "Login successfull",
-                        token : token
+                        token : token,
+                        role : user.role
                     })
                 }else{
                     res.status(404).json({
